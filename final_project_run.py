e2257bd47deaaa77d30cdf5c32d05729d56c77ac
fatmacankara <fatmacankaraa@gmail.com>
2022-01-12 03:31:34 +0300

    Add files via upload

---
+++ final_project_run.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+from scipy.integrate import solve_ivp
+import finalproject
+#from google.colab import drive
+
+
+def mytests():
+    finalproject.mytests()
+    plt.show()
+
+
+if __name__ == '__main__':
+    mytests()
